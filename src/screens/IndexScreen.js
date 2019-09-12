f8a38eadf77796e6498aa851740204fb89728f52
Jason <jason.mckelvey@companycam.com>
2019-09-11 20:43:13 -0500

    Set up initial navigation and Index Screen component

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({});
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+export default IndexScreen;
