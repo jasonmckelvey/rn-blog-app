08359523cefe462468995eef4b521805d7e26dcd
Jason <jason.mckelvey@companycam.com>
2019-09-11 21:23:24 -0500

    Added blog post title data to render from context into flatlist from index screen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { FlatList, View, Text, StyleSheet } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 const styles = StyleSheet.create({});
 
 const IndexScreen = () => {
+  const blogPosts = useContext(BlogContext);
   return (
     <View>
       <Text>Index Screen</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
