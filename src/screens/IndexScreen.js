52900a7eca941959bec14a86e954424739882022
Jason <jason.mckelvey@companycam.com>
2019-09-11 21:38:13 -0500

    Added button to update state whenever a blogpost is added

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,16 +1,17 @@
 import React, { useContext } from "react";
-import { FlatList, View, Text, StyleSheet } from "react-native";
+import { FlatList, View, Text, StyleSheet, Button } from "react-native";
 import BlogContext from "../context/BlogContext";
 
 const styles = StyleSheet.create({});
 
 const IndexScreen = () => {
-  const blogPosts = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
   return (
     <View>
       <Text>Index Screen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
-        data={blogPosts}
+        data={data}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
