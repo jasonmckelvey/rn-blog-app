fa99cee8dca605f04c0cbb54be21b1ad17d0710c
Jason <jason.mckelvey@companycam.com>
2019-09-13 14:47:33 -0500

    Added ability to delete blogposts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,20 +1,52 @@
 import React, { useContext } from "react";
-import { FlatList, View, Text, StyleSheet, Button } from "react-native";
+import {
+  FlatList,
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableOpacity
+} from "react-native";
 import { Context } from "../context/BlogContext";
 
-const styles = StyleSheet.create({});
+import { Feather } from "@expo/vector-icons";
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 16,
+    borderBottomWidth: 0.5,
+    borderColor: "#E0E4E6"
+  },
+  icon: {
+    color: "#4F5E68"
+  },
+  title: {
+    fontSize: 18
+  }
+});
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather name="trash" size={24} style={styles.icon} />
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </View>
