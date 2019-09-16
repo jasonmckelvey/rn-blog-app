a14d1d91a01034d7bd4c74bdb55a1fbf908a9dd4
Jason <jason.mckelvey@companycam.com>
2019-09-16 10:26:04 -0500

    Initialize state using context for edit screen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,13 +1,53 @@
-import React, { useContext } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React, { useState, useContext } from "react";
+import { View, Text, TextInput, StyleSheet, Button } from "react-native";
 import { Context } from "../context/BlogContext";
 import { Feather } from "@expo/vector-icons";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  wrapper: {
+    padding: 16
+  },
+  inputWrapper: {
+    marginBottom: 16
+  },
+  input: {
+    fontSize: 16,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#E8EDF1"
+  }
+});
+
 const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    blogPost => blogPost.id === navigation.getParam("id")
+  );
+
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
-    <View>
-      <Text>Edit - {navigation.getParam("id")}</Text>
+    <View style={styles.wrapper}>
+      <View style={styles.inputWrapper}>
+        <Text style={{ marginBottom: 4, color: "#5F738C" }}>Edit Title:</Text>
+        <TextInput
+          style={styles.input}
+          value={title}
+          onChangeText={newTitle => setTitle(newTitle)}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={{ marginBottom: 4, color: "#5F738C" }}>Edit Content:</Text>
+        <TextInput
+          style={styles.input}
+          value={content}
+          onChangeText={newContent => setContent(newContent)}
+        />
+      </View>
+      <Button title="Add Blog Post" />
     </View>
   );
 };
