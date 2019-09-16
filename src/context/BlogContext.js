ce42d19b85319ad8c41583946331d4521ec08cfd
Jason <jason.mckelvey@companycam.com>
2019-09-16 14:23:25 -0500

    Updated edit request

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -11,15 +11,7 @@ const blogReducer = (state, action) => {
       });
     case "delete_blogpost":
       return state.filter(blogPost => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ];
+
     default:
       return state;
   }
@@ -37,7 +29,6 @@ const addBlogPost = dispatch => {
   return async (title, content, callback) => {
     await jsonServer.post("/blogposts", { title, content });
 
-    // dispatch({ type: "add_blogpost", payload: { title, content } });
     if (callback) {
       callback();
     }
@@ -45,13 +36,16 @@ const addBlogPost = dispatch => {
 };
 
 const deleteBlogPost = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
+
     dispatch({
       type: "edit_blogpost",
       payload: { id, title, content }
